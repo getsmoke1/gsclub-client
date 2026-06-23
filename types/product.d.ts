26063c2599ac589f6e2d4fde6f47aa8c446df639
GetSmoke Dev <dev@getsmoke.com>
2026-06-23 12:35:54 +0800

    fix: stockStatus optional type to handle null from existing products

--- types/product.d.ts
+++ types/product.d.ts
@@ -95,7 +95,7 @@ export interface Product {
   extra: string | null;
   // Relations
   isArchived: boolean;
-  stockStatus: "INSTOCK" | "OUTOFSTOCK" | "PREORDER";
+  stockStatus?: "INSTOCK" | "OUTOFSTOCK" | "PREORDER" | null;
   brandId: string;
   brand: Brand;
   flavorId: string | null; // Make flavorId optional since it might use productFlavors instead

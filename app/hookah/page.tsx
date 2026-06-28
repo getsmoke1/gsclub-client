8b5782f88e4a9deb75265c9cf77d8bc145535a7a
GetSmoke Dev <dev@getsmoke.com>
2026-06-28 08:15:34 +0800

    fix: hookah page force-dynamic; fix geek-bar-pulse comparison article links

--- app/hookah/page.tsx
+++ app/hookah/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { Metadata } from "next";
 import VapePage from "@/components/vapes/VapePage";
 import { prefetchProducts } from "@/lib/prefetch-products";
